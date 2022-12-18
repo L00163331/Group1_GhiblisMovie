e7db7eb037dbd120bbd406d67297e2286c50cb44
L00171222 <L00171222@atu.ie>
2022-12-18 15:40:01 +0000

    backend changes 1

--- Backend/register.js
+++ Backend/register.js
@@ -2,7 +2,8 @@ const AWS = require('aws-sdk');
 AWS.config.update({
     region:'us-east-1'
 })
-const util = require('./utils')
+const util = require('./utils');
+const bcypt = require('bcryptjs');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 const userTable= 'prototype-username';
 
@@ -13,6 +14,48 @@ async function register(userInfo){
     const password = userinfo.password;
 
     if(!username || !name || !email || !password){
-       return  
+       return  util.buildResponse(401,{
+        message: 'All fields are required'
+       } )
     }
 }
+
+    const dynamoUser = await getUser(username.toLowerCase().trim());
+    if (dynamoUser && dynamoUser.username){
+        return util.buildResponse(401, {
+            message: 'username already exists in our database, please choose a different username'
+        })
+    }
+
+    const encryptedPW = bcypt.hashSync(password.trim(), 10);
+    const user = {
+        name: name,
+        email: email,
+        username: username.toLowerCase().trim(),
+        password: encryptedPW
+    }
+
+    const saveUserResponse = await saveUser(user);
+    if(!saveUserResponse){
+        return util.buildResponse(503, { message: 'Server Error'});
+    }
+        return util.buildResponse(200, { username: username});
+
+    async function getUser(username){
+        const params = {
+            TableName: userTable,
+            Key: {
+                username: username
+            }
+        }
+    }    
+
+    return await dynamodb.get(params).promise().then(response => {
+        return response.Item;
+    }, error => {
+        console.error("There is an error getting user: ", error);
+    })
+
+    return await dynamodb.put(params).promise().then(() => {
+        return true; 
+    }, error=> {console.error('Ther is an error saving user:', error)});
